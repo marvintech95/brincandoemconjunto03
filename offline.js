2c46f6d7279e18a2450af4ad36bf75885013c14f
marvintech95 <66658788+marvintech95@users.noreply.github.com>
2020-06-09 18:49:09 -0300

    Upload da versão 1v1 do jogo
    Inseridas todas as imagens, realizadas as devidas correções e inseridas as funções e eventos necessários para alguns pontos importantes do jogo.

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1591739139,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"tiledbackground2.png",
+		"tiledbackground.png",
+		"spritebuttoninfo-sheet0.png",
+		"tiledbackground3.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"spritebox-sheet0.png",
+		"sprite5-sheet0.png",
+		"spritebuttonstart-sheet0.png",
+		"sprite6-sheet0.png",
+		"sprite12-sheet0.png",
+		"sprite8-sheet0.png",
+		"sprite13-sheet0.png",
+		"sprite14-sheet0.png",
+		"sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
